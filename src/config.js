ccd5733e646436b11c3efd561e8c43ea2231ee88
toptecshare <eestimorol.ice9208g@gmail.com>
2022-01-18 19:05:12 -0500

    COMPLETE

--- src/config.js
+++ src/config.js
@@ -32,40 +32,49 @@ const numberToProduce = {
   mh_o3: 2,
   mh_o4: 1,
 };
+const defaultEdition = 12000;
+
+const numberToProduce = {
+  mh: 5,
+  mh_o1: 4,
+  mh_o2: 3,
+  mh_o3: 2,
+  mh_o4: 1,
+};
 
 const totalSupply = {
   mh: {
-    common: 24,
-    uncommon: 120,
-    rare: 6,
-    superrare: 80,
+    common: 2440,
+    uncommon: 1240,
+    rare: 640,
+    superrare: 800,
     legendary: 0,
   },
 
   mh_o1: {
-    common: 180,
-    uncommon: 90,
-    rare: 40,
+    common: 1830,
+    uncommon: 930,
+    rare: 480,
     superrare: 0,
-    legendary: 1,
+    legendary: 160,
   },
   mh_o2: {
-    common: 9,
-    uncommon: 4,
-    rare: 2,
+    common: 915,
+    uncommon: 465,
+    rare: 240,
     superrare: 0,
-    legendary: 1,
+    legendary: 100,
   },
   mh_o3: {
-    common: 60,
-    uncommon: 30,
-    rare: 1,
+    common: 610,
+    uncommon: 310,
+    rare: 160,
     superrare: 0,
-    legendary: 8,
+    legendary: 80,
   },
   mh_o4: {
-    common: 30,
-    uncommon: 10,
+    common: 305,
+    uncommon: 155,
     rare: 80,
     superrare: 0,
     legendary: 60,
